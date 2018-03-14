47e46958f4157715577cdac5a74cd8c2dc7cafdd
Henning Ward <henningward@dhcp-10-22-126-19.wlan.ntnu.no>
2018-03-14 14:31:18 +0100

    replaced old unity-henning with a new and perfect one

---
+++ Unity- Henning/Assets/scripts/move_camera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_camera : MonoBehaviour {
+
+	private float i = 0f;
+	private float time_interval = 0.001f;
+
+	private Vector3 start;
+	private Vector3 end;
+
+	// Use this for initialization
+	void Start () {
+		
+		InvokeRepeating("update_position", 1.0f, time_interval);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void update_position(){
+		Vector3 start = Camera.main.transform.position;
+		Vector3 end = new Vector3(transform.position.x-i, transform.position.y, transform.position.z);
+		//Camera.main.transform.position = new Vector3.Lerp(start, end, time_interval);
+		Camera.main.transform.position = new Vector3(start.x+i, start.y, start.z);
+		i-= time_interval/100;
+	}
+}
--- Unity- Henning/Assets/scripts/path_plotter.cs
+++ Unity- Henning/Assets/scripts/path_plotter.cs
@@ -6,11 +6,13 @@ using System;
 using System.IO;
 
 
-public class kamera : MonoBehaviour {
+public class path_plotter : MonoBehaviour {
 
 	// Use this for initialization
 
-	public GameObject testste;
+	public GameObject path_object;
+	private Transform temp_obj;
+	private Transform prev_obj;
 
 	public int current_x = 50;
 	public int current_y = 20;
@@ -19,20 +21,31 @@ public class kamera : MonoBehaviour {
 	Vector3 temp = new Vector3(0,0,2);
 
 	void Start () {
-		Transform Transformed = testste.transform;
+		Transform Transformed = path_object.transform;
+
 
 		string[] lines = System.IO.File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "coordinates.txt"));
 
 		foreach (string line in lines)
 		{
-			
 			string[] tokens = line.Split(' ');
-			Debug.Log (tokens[1] +"\n");
-			Instantiate(Transformed, new Vector3(Int32.Parse(tokens[0]) - current_x, 0, Int32.Parse(tokens[1]) - current_y), Quaternion.identity);
-			 
+			//Debug.Log (tokens[1] +"\n");
+			temp_obj = Instantiate(Transformed, new Vector3(Int32.Parse(tokens[0]) - current_x, 0, Int32.Parse(tokens[1]) - current_y), Quaternion.identity);
+			temp_obj.localScale = new Vector3 (.8f, .8f, .8f);
+
+			if (prev_obj != null) {
+				prev_obj.LookAt (temp_obj);
+				prev_obj.transform.Rotate (Vector3.up * 90);
+				prev_obj.transform.Rotate(Vector3.right * 90);
+				// rotate the previous arrow
+			}
+
+			prev_obj = temp_obj;
+
+
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		//if (testobj.position.magnitude <= 3){
